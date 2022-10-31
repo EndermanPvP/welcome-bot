e95ced285ff35eeaf55295ffb6d7619c002e17e4
EndermanPvP <100229122+EndermanPvP@users.noreply.github.com>
2022-10-31 09:47:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const leave = require("./leave")
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_PRESENCES", "GUILD_MEMBERS", "GUILD_MESSAGE_REACTIONS"]});
 
 client.on("ready", () => {
-    console.log("Bot made by EndermanPvP#0001!"/*Do not remove credits.*/);
+    console.log("The but is now online!", "Bot made by EndermanPvP#0001!"/*Do not remove credits*/);
     welcome(client);
-    leave(client);
+    bye(client);
 });
 
 client.login("You bot token")
