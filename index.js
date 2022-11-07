97d6c4002a7fdbc5df85b932ee27277763a6222c
EndermanPvP <100229122+EndermanPvP@users.noreply.github.com>
2022-11-06 21:28:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const welcome = require("./welcome")
-const leave = require("./leave")
+const bye = require("./bye")
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_PRESENCES", "GUILD_MEMBERS", "GUILD_MESSAGE_REACTIONS"]});
 
 client.on("ready", () => {
