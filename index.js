47a22f65a28924204d743363eb0b8103e2f0693b
EndermanPvP <100229122+EndermanPvP@users.noreply.github.com>
2022-12-14 12:45:16 -0700

    Renamed Bye.js to Leave.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 const welcome = require("./welcome")
-const bye = require("./bye")
+const leave = require("./leave")
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_PRESENCES", "GUILD_MEMBERS", "GUILD_MESSAGE_REACTIONS"]});
 
 client.on("ready", () => {
     console.log("The but is now online!", "Bot made by https://github.com/EndermanPvP"/*Do not remove credits*/);
     welcome(client);
-    bye(client);
+    leave(client);
 });
 
 client.login("You bot token")
